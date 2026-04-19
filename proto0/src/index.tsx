fb3b8269f491d15648b771141479b74f9ee0102e
yahiro <yahiro07@users.noreply.github.com>
2026-04-19 21:37:43 +0900

    create game pad ui

--- proto0/src/index.tsx
+++ proto0/src/index.tsx
@@ -3,24 +3,132 @@ import "./styling/page.css";
 import "./styling/utility-classes.css";
 import { createStore } from "snap-store";
 
-const store = createStore<{ count: number }>({
-  count: 0,
+const maxStep = 64;
+
+const store = createStore<{ cursorPos: number; cursorDuration: number }>({
+  cursorPos: 0,
+  cursorDuration: 2,
 });
 
+const durationValues = [4, 2, 1];
+
+const actions = {
+  dummy() {},
+  shiftCursorPos(dir: -1 | 1) {
+    const { cursorDuration } = store.state;
+    store.mutations.setCursorPos((prev) => {
+      return (prev + dir * cursorDuration + maxStep) % maxStep;
+    });
+  },
+  shiftCursorPosV(dir: -1 | 1) {
+    const amount = 16;
+    store.mutations.setCursorPos((prev) => {
+      return (prev + dir * amount + maxStep) % maxStep;
+    });
+  },
+  shiftDuration(dir: -1 | 1) {
+    const { cursorDuration } = store.state;
+    const idx = durationValues.indexOf(cursorDuration);
+    const newIdx = (idx + dir + durationValues.length) % durationValues.length;
+    store.mutations.setCursorDuration(durationValues[newIdx]);
+  },
+};
+
+function durationToString(d: number) {
+  if (d === 1) return "/16";
+  if (d === 2) return "/8";
+  if (d === 4) return "/4";
+  return String(d);
+}
+
+const Button = ({
+  active,
+  text,
+  children,
+  onClick,
+}: {
+  active?: boolean;
+  text?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="min-w-[40px] h-[40px]"
+      css={{
+        border: "solid 1px #888",
+        backgroundColor: active ? "#ddd" : "#fff",
+        borderRadius: "50%",
+        cursor: "pointer",
+      }}
+    >
+      {text && <span>{text}</span>}
+      {children}
+    </button>
+  );
+};
+
 const EditorArea = () => {
-  const { count } = store.useSnapshot();
+  const st = store.useSnapshot();
   return (
     <div
       css={{
-        width: "400px",
-        height: "400px",
+        width: "200px",
+        height: "200px",
         border: "solid 1px #888",
       }}
     >
-      <p>Count: {count}</p>
-      <button type="button" onClick={() => store.setCount((c) => c + 1)}>
-        inc
-      </button>
+      <div>pos: {st.cursorPos}</div>
+      <div>duration: {st.cursorDuration}</div>
+      <div>durationStr: {durationToString(st.cursorDuration)}</div>
+    </div>
+  );
+};
+
+const LeftControlArea = () => {
+  return (
+    <div className="flex-ha">
+      <div>
+        <Button text="←" onClick={() => actions.shiftCursorPos(-1)} />
+      </div>
+      <div className="flex-v">
+        <Button text="↑" onClick={() => actions.shiftCursorPosV(-1)} />
+        <div className="h-[40px]" />
+        <Button text="↓" onClick={() => actions.shiftCursorPosV(1)} />
+      </div>
+      <div>
+        <Button text="→" onClick={() => actions.shiftCursorPos(1)} />
+      </div>
+    </div>
+  );
+};
+
+const RightControlArea = () => {
+  return (
+    <div className="flex-ha">
+      <div>
+        <Button text="dur" onClick={() => actions.shiftDuration(1)} />
+      </div>
+      <div className="flex-v">
+        <Button text="edit" onClick={() => actions.dummy()} />
+        <div className="h-[40px]" />
+        <Button text="rest" onClick={() => actions.dummy()} />
+      </div>
+      <div>
+        <Button text="tie" onClick={() => actions.dummy()} />
+      </div>
+    </div>
+  );
+};
+
+const PanelBody = () => {
+  return (
+    <div className="flex-ha gap-4 p-5 border border-[#888] rounded-[999px]">
+      <LeftControlArea />
+      <EditorArea />
+      <RightControlArea />
     </div>
   );
 };
@@ -28,7 +136,7 @@ const EditorArea = () => {
 const App = () => {
   return (
     <div className="flex-c" css={{ width: "100vw", height: "100vh" }}>
-      <EditorArea />
+      <PanelBody />
     </div>
   );
 };
