6835df63812a940f2e91c91a6df92089104b5c9a
yahiro <yahiro07@users.noreply.github.com>
2026-04-29 07:34:30 +0900

    send unit active state to sequencer

--- series2/projects/fsq-d0/index.tsx
+++ series2/projects/fsq-d0/index.tsx
@@ -5,28 +5,37 @@ import { Button } from "@lib/components1/button";
 import { useEffect } from "react";
 import { createStore } from "snap-store";
 
+type SequencerCommand =
+  | { type: "start" }
+  | { type: "stop" }
+  | { type: "setUnitActive"; unitId: string; active: boolean };
+
 function createSequencer(soundEngine: SoundEngine) {
   let timerId: number;
 
   let frameCount = 0;
 
+  let fish1Active = false;
+
   const handleTick = () => {
     frameCount++;
-    if (frameCount % 20 === 0) {
-      soundEngine.playNote(9, 36, 100);
-    }
-    if (frameCount % 20 === 10) {
-      soundEngine.playNote(9, 36, 0);
-    }
-    if (frameCount % 20 === 10) {
-      soundEngine.playNote(9, 38, 100);
-    }
-    if (frameCount % 20 === 15) {
-      soundEngine.playNote(9, 38, 0);
+    if (fish1Active) {
+      if (frameCount % 20 === 0) {
+        soundEngine.playNote(9, 36, 100);
+      }
+      if (frameCount % 20 === 10) {
+        soundEngine.playNote(9, 36, 0);
+      }
+      if (frameCount % 20 === 10) {
+        soundEngine.playNote(9, 38, 100);
+      }
+      if (frameCount % 20 === 15) {
+        soundEngine.playNote(9, 38, 0);
+      }
     }
   };
 
-  return {
+  const internal = {
     start() {
       timerId = setInterval(handleTick, 50);
     },
@@ -34,32 +43,61 @@ function createSequencer(soundEngine: SoundEngine) {
       clearInterval(timerId);
     },
   };
+
+  return {
+    handelCommand(command: SequencerCommand) {
+      if (command.type === "start") internal.start();
+      if (command.type === "stop") internal.stop();
+      if (command.type === "setUnitActive") {
+        if (command.unitId === "fish1") {
+          fish1Active = command.active;
+        }
+      }
+    },
+  };
 }
 
 const soundEngine = await setupSoundEngine();
 const sequencer = createSequencer(soundEngine);
 
-const store = createStore<{ programNumber: number; playing: boolean }>({
+const store = createStore<{
+  programNumber: number;
+  playing: boolean;
+  fish1Active: boolean;
+}>({
   playing: false,
   programNumber: 48,
+  fish1Active: false,
 });
 soundEngine.selectProgram(0, store.state.programNumber);
 
-store.subscribe((attrs) => {
-  if (attrs.programNumber !== undefined) {
-    soundEngine.selectProgram(0, attrs.programNumber);
+store.subscribe(({ programNumber, playing, fish1Active }) => {
+  if (programNumber !== undefined) {
+    soundEngine.selectProgram(0, programNumber);
+  }
+  if (playing !== undefined) {
+    if (playing) {
+      sequencer.handelCommand({ type: "start" });
+    } else {
+      sequencer.handelCommand({ type: "stop" });
+    }
+  }
+  if (fish1Active !== undefined) {
+    sequencer.handelCommand({
+      type: "setUnitActive",
+      unitId: "fish1",
+      active: fish1Active,
+    });
   }
 });
+store.setFish1Active(true);
 
 const uiActions = {
   togglePlayState() {
-    if (!store.state.playing) {
-      sequencer.start();
-      store.mutations.setPlaying(true);
-    } else {
-      sequencer.stop();
-      store.mutations.setPlaying(false);
-    }
+    store.mutations.togglePlaying();
+  },
+  toggleFish1Active() {
+    store.mutations.toggleFish1Active();
   },
   async handleNote(noteNumber: number, velocity: number) {
     await soundEngine.resumeIfNeed();
@@ -77,6 +115,12 @@ const MainPanel = () => {
       <div className="flex-ha gap-2">
         <Button active={st.playing} onClick={() => uiActions.togglePlayState()}>
           play
+        </Button>
+        <Button
+          active={st.fish1Active}
+          onClick={() => uiActions.toggleFish1Active()}
+        >
+          fish1
         </Button>
       </div>
       <div className="flex-v gap-2">
