471d1dad413893af57b5195f4dedbb3219918b52
yahiro <yahiro07@users.noreply.github.com>
2026-05-04 22:52:11 +0900

    add valiation

--- sr2605/series3-fm-synth/projects/ds9-fm-synth/x/dev1-note-pitch.tsx
+++ sr2605/series3-fm-synth/projects/ds9-fm-synth/x/dev1-note-pitch.tsx
@@ -37,10 +37,90 @@ function NoteInputBar_Normal() {
   );
 }
 
+function NoteInputBar_InScale() {
+  return (
+    <div class="flex-v">
+      {seqNumbers(22).map((i) => {
+        const ni = 21 - i - 7;
+        const highlighted = ni % 7 === 0;
+        return (
+          <div
+            class="w-[60px] h-[20px] border border-[#888] flex-c text-[#888]"
+            style={{ background: highlighted ? "#ddf" : "#fff" }}
+          >
+            {ni}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function NoteInputBar_Narrow1() {
+  return (
+    <div class="flex-v">
+      {seqNumbers(13).map((i) => {
+        const ni = (12 - i - 4) * 3;
+        const highlighted = ni % 12 === 0;
+        return (
+          <div
+            class="w-[60px] h-[20px] border border-[#888] flex-c text-[#888]"
+            style={{ background: highlighted ? "#ddf" : "#fff" }}
+          >
+            {ni}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function NoteInputBar_Narrow2() {
+  return (
+    <div class="flex-v">
+      {seqNumbers(10).map((i) => {
+        const ni = (9 - i - 3) * 4;
+        const highlighted = ni % 12 === 0;
+        return (
+          <div
+            class="w-[60px] h-[20px] border border-[#888] flex-c text-[#888]"
+            style={{ background: highlighted ? "#ddf" : "#fff" }}
+          >
+            {ni}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function NoteInputBar_Narrow3() {
+  return (
+    <div class="flex-v">
+      {seqNumbers(7).map((i) => {
+        const ni = (6 - i - 2) * 6;
+        const highlighted = ni % 12 === 0;
+        return (
+          <div
+            class="w-[60px] h-[20px] border border-[#888] flex-c text-[#888]"
+            style={{ background: highlighted ? "#ddf" : "#fff" }}
+          >
+            {ni}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 function MainUi() {
   return (
     <div class="w-dvw h-dvh flex-c gap-2">
       <NoteInputBar_Normal />
+      <NoteInputBar_InScale />
+      <NoteInputBar_Narrow1 />
+      <NoteInputBar_Narrow2 />
+      <NoteInputBar_Narrow3 />
     </div>
   );
 }
